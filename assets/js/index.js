3fcb6104216267d6cbd35dcbc57d06477bf48617
LiChao <lc2510834510@gamil.com>
2018-07-04 23:23:09 +0800

    q

--- assets/js/index.js
+++ assets/js/index.js
@@ -162,8 +162,8 @@ var fruit = new Vue({
     data: {
         fruits: [{
                 id: 1,
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/482065238502496930.jpg',
+                name: '美人李',
                 kind: 'banana',
                 price: 5.5,
                 shopper: {
@@ -174,12 +174,12 @@ var fruit = new Vue({
                     id: 1,
                     name: '中国'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '一树红美人，枝繁叶茂间点缀着漫天的朱红。我们闻见了李子独有的香味混合着灌木清香。靠近果树，扑鼻的美人李香迎面袭来。在阳光的照耀下，红得晶莹剔透。'
             },
             {
                 id: 2,
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/917593610264255430.jpg',
+                name: '甜心芒',
                 kind: 'banana',
                 price: 5,
                 shopper: {
@@ -190,12 +190,12 @@ var fruit = new Vue({
                     id: 2,
                     name: '越南'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '甜心芒活动9斤20个以内，一个4两左右。'
             },
             {
                 id: 3,
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/33558913801732740.jpg',
+                name: '南非西柚',
                 kind: 'banana',
                 price: 5,
                 shopper: {
@@ -206,12 +206,12 @@ var fruit = new Vue({
                     id: 3,
                     name: '荷兰'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '一件8颗，单果230-300克买6颗送2颗，实发8颗，55包邮‼'
             },
             {
                 id: 4,
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/256660426650346801.jpg',
+                name: '纽扣蟠桃',
                 kind: 'banana',
                 price: 5,
                 shopper: {
@@ -222,12 +222,12 @@ var fruit = new Vue({
                     id: 4,
                     name: '美国'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '今天最后一天接单，错过再等一年， 5斤，49.9包邮'
             },
             {
                 id: 5,
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/659479878259961007.jpg',
+                name: '无核荔枝',
                 kind: 'banana',
                 price: 5,
                 shopper: {
@@ -238,12 +238,12 @@ var fruit = new Vue({
                     id: 1,
                     name: '中国'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '“树上的蜂蜜，汁液爆满” 听说咬下去会有要爆的感觉'
             },
             {
                 id: 6,
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/823185833255645548.jpg',
+                name: '夏橙',
                 kind: 'banana',
                 price: 5,
                 shopper: {
@@ -254,11 +254,11 @@ var fruit = new Vue({
                     id: '2',
                     name: '越南'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '喜欢橙子，那是最接近太阳的颜色 【夏橙】以夏命名的水果  个头不大，果皮薄，果肉厚实 汁多不腻，甜中带酸，橙味浓。'
             },
             {
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/198765557630413770.jpg',
+                name: '黄蜜樱桃',
                 kind: 'banana',
                 price: 5,
                 shopper: {
@@ -269,10 +269,10 @@ var fruit = new Vue({
                     id: 3,
                     name: '荷兰'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '纯正的黄蜜樱桃，皮薄 甜美美的颜色绚烂整个樱桃🍒季节 好看'
             }, {
-                src: '../Sfruit/Images/banana.jpg',
-                name: '香蕉',
+                src: '../Sfruit/Images/415323767774883523.jpg',
+                name: '无籽青柠檬',
                 kind: 'banana',
                 price: 5,
                 shopper: {
@@ -283,7 +283,7 @@ var fruit = new Vue({
                     id: 4,
                     name: '美国'
                 },
-                jieshao: '这是香蕉。'
+                jieshao: '【夏日伴侣】泰国无籽青柠檬正宗泰国青柠檬，柠檬中的优质产品，本品皮薄多汁，气味清香，是茶餐厅、冷饮店不可或缺的原料；是美容美白，夏季解暑的必备良品；不打蜡，每个都经过精挑细选才发货一斤约7个。'
             }, {
                 src: '../Sfruit/Images/Kiwi.jpg',
                 name: '猕猴桃',
